9c537e5fa6774fc0ed4712ad28d9a9ff650aaf46
Your Name <you@example.com>
2021-08-12 18:36:01 +0300

    Bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,10 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
-                radius: 50.0,
+                radius: 70.0,
                 backgroundImage:
                     NetworkImage('https://i.hizliresim.com/65N8rv.jpg'),
               ),
@@ -35,6 +36,53 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'SourceSansPro',
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.local_phone_outlined,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+90 594 712 51 56',
+                      style: TextStyle(
+                        fontFamily: 'OpenSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email_outlined,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'mehmetalidemir99mad@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'OpenSansPro',
+                      color: Colors.teal.shade900,
+                      fontSize: 17.0,
+                    ),
+                  ),
                 ),
               ),
             ],
